54c25eefd0592e1348eebb4fc82aa40d68027a18
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-07-07 23:53:37 +0500

    first touch

---
+++ src/main/java/docs/openapi/SpringBoot3SwaggerUiApplication.java
@@ -0,0 +1,13 @@
+package docs.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3SwaggerUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SwaggerUiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docs/openapi/SpringBoot3SwaggerUiApplicationTests.java
@@ -0,0 +1,13 @@
+package docs.openapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SwaggerUiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
