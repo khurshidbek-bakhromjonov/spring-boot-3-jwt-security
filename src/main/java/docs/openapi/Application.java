19a50502d18e5d780dbc398ee6d9d178bfa64696
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-07-08 00:10:33 +0500

    renaming the project

--- src/main/java/docs/openapi/Application.java
+++ src/main/java/docs/openapi/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBoot3SwaggerUiApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBoot3SwaggerUiApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
