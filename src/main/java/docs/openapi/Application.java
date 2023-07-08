44b728928bab207d8e8feaf2127fe76f4d4b6038
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-07-08 17:10:34 +0500

    CommandLineRunner

--- src/main/java/docs/openapi/Application.java
+++ src/main/java/docs/openapi/Application.java
@@ -1,10 +1,14 @@
 package docs.openapi;
 
 import docs.openapi.auth.AuthenticationService;
+import docs.openapi.auth.RegisterRequest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import static docs.openapi.user.Role.ADMIN;
+import static docs.openapi.user.Role.MANAGER;
 
 @SpringBootApplication
 public class Application {
@@ -15,7 +19,25 @@ public class Application {
 
     @Bean
     public CommandLineRunner commandLineRunner(AuthenticationService authenticationService) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + authenticationService.register(admin).getAccessToken());
 
+            var manager = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("manager@mail.com")
+                    .password("password")
+                    .role(MANAGER)
+                    .build();
+            System.out.println("Manager token: " + authenticationService.register(manager).getAccessToken());
+        };
     }
 
 }
--- src/main/java/docs/openapi/auth/RegisterRequest.java
+++ src/main/java/docs/openapi/auth/RegisterRequest.java
@@ -1,11 +1,13 @@
 package docs.openapi.auth;
 
 import docs.openapi.user.Role;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class RegisterRequest {
 
     private String firstname;
